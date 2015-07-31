993c9a9a6783e60aec4a2d910c86c2048b5429a0
ungki <ungki.h@gmail.com>
2015-08-01 00:09:32 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,227 @@
-var browsersync = require('browser-sync');
-var reload      = browsersync.reload;
-var gulp        = require('gulp');
+'use strict';
+
+var banner = [
+  '/*!',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * Version <%= pkg.version %>',
+  ' * <%= pkg.homepage %>',
+  ' * Author : <%= pkg.author %>',
+  ' * License : <%= pkg.license %>',
+  ' */',
+  ''
+].join('\n');
+
+var del = require('del');
+var fs = require('fs');
+var ghpages = require('gulp-gh-pages');
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var pagespeed = require('psi');
+var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
-var autoprefixerBrowsers = ['last 2 version']
 
-module.exports = {
-  'browsersync': {
-    server: './gh-pages',
-    open: 'external'
-  },
-  'uninstall': {
-    files: [
-      './gh-pages',
-      './src/css'
-    ]
-  },
-  'rubysass': {
-    src: './src/scss/docs.scss',
-    dest: './src/css',
-    rubySassOptions: {
-      sourcemap: true,
-      noCache: true
-    },
-    autoprefixer: autoprefixerBrowsers,
-    fallback:false,
-    filter:'**/*.css',
-    headerBanner : false,
-    notify :"Compiled Sass"
-  },
-  'ghpage' : {
-    src : './gh-pages/**/*',
-    remoteUrl : 'git@github.com:blivesta/animsition.git',
-    branch : 'gh-pages'
-  },
-};
+var browserify = require('browserify');
+var jshint = require('gulp-jshint');
+var source = require('vinyl-source-stream');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
 
-gulp.task('rubysass', require('gulptasks/lib/rubysass'));
-gulp.task('deploy', require('gulptasks/lib/ghpage'));
-gulp.task('jekyll', require('gulptasks/lib/jekyll'));
-gulp.task('browsersync', require('gulptasks/lib/browsersync'));
-gulp.task('uninstall', require('gulptasks/lib/uninstall'));
+var cssnext = require('gulp-cssnext');
+var header = require('gulp-header');
 
-gulp.task('default',['browsersync'],function() {
-  gulp.watch(['./src/scss/*.scss'], ['rubysass']);
-  gulp.watch(['./src/**/*.{html,css,js}'], ['jekyll']);
-  gulp.watch(['./gh-pages'], reload);
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var fm = require('gulp-front-matter');
+var sitemap = require('gulp-sitemap');
+var hb = require('gulp-hb');
+var htmlmin = require('gulp-htmlmin');
+var htmlhint = require("gulp-htmlhint");
+var yaml = require('js-yaml');
+
+// ----------------------------------------------------------------
+
+gulp.task('html', function() {
+  return gulp
+    .src(['src/**/*.hbs', '!src/{partials,partials/**}'])
+    .pipe(fm({
+      property: 'meta'
+    }))
+    .pipe(hb({
+      bustCache: true,
+      debug: false,
+      data: {
+        pkg: pkg,
+        site: yaml.safeLoad(fs.readFileSync('./site.yml', 'utf8'))
+      },
+      helpers: './helpers.js',
+      partials: './src/partials/**/*.hbs'
+    }))
+    .pipe(rename(function(path) {
+      if (path.basename == 'index') {
+        path.extname = '.html';
+      } else {
+        path.dirname = (path.dirname ? path.dirname + '/' : '') + path.basename;
+        path.basename = 'index';
+        path.extname = '.html';
+      }
+    }))
+    .pipe(gulp.dest('./gh-pages'));
 });
 
-gulp.task('build', function() {
+gulp.task('htmlmin', function() {
+  return gulp
+    .src('./gh-pages/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./gh-pages'));
+});
+
+gulp.task('htmlhint', function() {
+  return gulp
+    .src('./gh-pages/**/*.html')
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter('htmlhint-stylish'));
+});
+
+gulp.task('sitemap', function() {
+  return gulp
+    .src('./gh-pages/**/*.html')
+    .pipe(sitemap({
+      siteUrl: pkg.homepage
+    }))
+    .pipe(gulp.dest('./gh-pages'));
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('js', function() {
+  return browserify('./src/js/' + pkg.name + '.js', {
+      debug: true
+    })
+    .bundle()
+    .on('error', function(err) {
+      console.log('Error : ' + err.message);
+      this.emit('end');
+    })
+    .pipe(source(pkg.name + '.js'))
+    .pipe(gulp.dest('./gh-pages/js'));
+});
+
+gulp.task('jsmin', function() {
+  return gulp
+    .src('./gh-pages/js/' + pkg.name + '.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./gh-pages/js'));
+});
+
+gulp.task('jshint', function() {
+  return gulp
+    .src('./src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('css', function() {
+  return gulp
+    .src('./src/css/' + pkg.name + '.css')
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(cssnext({
+      browsers: ['last 2 versions'],
+      sourcemap: true
+    }))
+    .pipe(gulp.dest('./gh-pages/css'));
+});
+
+gulp.task('cssmin', function() {
+  return gulp
+    .src('./src/css/' + pkg.name + '.css')
+    .pipe(cssnext({
+      browsers: ['last 2 versions'],
+      compress: true
+    }))
+    .pipe(gulp.dest('./gh-pages/css'));
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './gh-pages',
+      open: 'external'
+    }
+  });
+  gulp.watch(['./src/css/**/*.css'], ['css']);
+  gulp.watch(['./src/js/**/*.js'], ['js']);
+  gulp.watch(['./src/**/*.hbs'], ['html']);
+  gulp.watch(['./gh-pages/{css,js}/*.{css,js}']).on('change', reload);
+  gulp.watch(['./gh-pages/*.html']).on('change', reload);
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('pagespeed', function() {
+  return pagespeed(pkg.homepage, {
+    nokey: 'true',
+    strategy: 'desktop',
+  }, function(err, data) {
+    console.log('Score: ' + data.score);
+    console.log('Page stats');
+    console.log(data.pageStats);
+  });
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('cleanup', function(cb) {
+  return del(['./gh-pages'], cb);
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('ghpages', function() {
+  return gulp
+    .src('./gh-pages/**/*')
+    .pipe(ghpages({
+      remoteUrl: 'git@github.com:blivesta/animsition.git',
+      branch: 'gh-pages'
+    }));
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('minify', ['cssmin', 'jsmin', 'htmlmin']);
+
+// ----------------------------------------------------------------
+
+gulp.task('default', ['build'], function(cb) {
   runSequence(
-    'uninstall',
-    ['rubysass'],
-    'jekyll',
-    ['default']
+    ['serve'],
+    cb
+  );
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('build', ['cleanup'], function(cb) {
+  runSequence(
+    'js', 'css', 'html', ['sitemap', 'jshint', 'htmlhint'],
+    cb
+  );
+});
+
+// ----------------------------------------------------------------
+
+gulp.task('deploy', ['build'], function(cb) {
+  runSequence(
+    ['minify'],
+    'ghpages', ['pagespeed'],
+    cb
   );
 });
